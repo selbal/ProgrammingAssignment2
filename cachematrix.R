c2a407e3df2c58cb671cf47d0316d19685d17212
selbal <stephaniebaravelli@gmail.com>
2018-09-28 15:30:59 +0100

    Programming Assignment wk 3
    Code to complete week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,44 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
+# new function with matrix arg(regmatrix)
+makeCacheMatrix <- function(regmatrix = matrix()) {
+	# create 'placeholder' for inverse matrix
+	inversematrix <- NULL
+	# create function to assign value to regmatrix. Reverse matrix still null for now.
+	set <- function(setvalue) {
+		regmatrix <<- setvalue
+		inversematrix <<- NULL
+	}
+	# create function to return regmatrix
+	get <- function() regmatrix
+	# create function to assign inverse matrix vals (opps)
+	setinvals <- function(opps) inversematrix <<- opps
+	# create function to return the above inverse vals
+	returninvals <- function() inversematrix
+	# multiple functions within a function can be referred to in a parentfunction$childfunction format if you specify those funcions in a list, as below
+	list(set = set, get = get, setinvals = setinvals, returninvals = returninvals)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(regmatrix, ...) {
+	# use assign returninvals to inversematrix
+	inversematrix <- regmatrix$returninvals()
+	# check if inversematirx is already populated (i.e. it's not true that it's null). if it is populated, return message and data
+	if (!is.null(inversematrix)) {
+		message("getting cached data")
+		return(inversematrix)
+	}
+	# if there aren't already values assigned
+	# get regmatrix values and assign to a variable
+data <- regmatrix$get()
+	# using solve, assign the inverse values of your new variable to inversematrix
+	inversematrix <- solve(data, ...)
+	# use the setinvals functiont to assign your inverse matrix in line with the above
+	regmatrix$setinvals(inversematrix)
+	# return inversematrix
+	inversematrix
 }
